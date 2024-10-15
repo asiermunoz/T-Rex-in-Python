9bf3f724af0ec6e04cc99bf357d7507104ab4743
asiermunoz <asiermunozlandi@gmail.com>
2024-10-15 11:18:17 -0400

    tests

--- main.py
+++ main.py
@@ -9,3 +9,6 @@ WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 GROUND_HEIGHT = SCREEN_HEIGTH - 70
 
+#create the game window
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGTH))
+pygame.display.set_caption("T-REX GAME")
