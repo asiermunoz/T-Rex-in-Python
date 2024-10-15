a037e91c5cc687bd9264864d0fa8f66688b90a03
asiermunoz <asiermunozlandi@gmail.com>
2024-10-15 10:59:27 -0400

    setting the screen colors and dimensions

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("hello wolrd")
+import pygame
+
+pygame.init()
+SCREEN_WIDTH = 800
+SCREEN_HEIGTH = 400
+
+#colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+GROUND_HEIGHT = SCREEN_HEIGTH - 70
+
